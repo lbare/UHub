19dba7764e0ff240e6c985addef1e66161e431d1
Bipasa Agrawal <bipasaagrawal@uvic.ca>
2024-02-15 13:18:16 -0800

    editing to add useEffect

--- App.tsx
+++ App.tsx
@@ -4,22 +4,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeNavigation from "./navigation/HomeNavigation";
 import { collection, addDoc } from "firebase/firestore"; 
 import {db} from "./src/firebase";
+import { useEffect } from "react";
 
 export default function App() {
-
-  async function create() {
-    try {
-      const docRef =  await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
+  useEffect(() => {
+    async function create() {
+      try {
+        const docRef =  await addDoc(collection(db, "users"), {
+          first: "Ada",
+          last: "Lovelace",
+          born: 1815
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
     }
-  }
-  create();
+    create();
+  }, []); 
   return (
     <NavigationContainer>
       <HomeNavigation />
