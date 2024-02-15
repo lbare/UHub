008b547dbef9ed41dc44a549a974f1027c8c4847
Bipasa Agrawal <bipasaagrawal@uvic.ca>
2024-02-15 13:41:34 -0800

    Adding example vendor

--- App.tsx
+++ App.tsx
@@ -5,17 +5,17 @@ import HomeNavigation from "./navigation/HomeNavigation";
 import { collection, addDoc } from "firebase/firestore"; 
 import {db} from "./src/firebase";
 import { useEffect } from "react";
+import data from "./src/data.json"; 
 
 export default function App() {
   useEffect(() => {
     async function create() {
       try {
-        const docRef =  await addDoc(collection(db, "users"), {
-          first: "Ada",
-          last: "Lovelace",
-          born: 1815
-        });
+        for (const vendorKey in data) {
+          const vendor = data[vendorKey];
+          const docRef = await addDoc (collection(db, "FoodVendor"), vendor)
         console.log("Document written with ID: ", docRef.id);
+        }
       } catch (e) {
         console.error("Error adding document: ", e);
       }
