c0fae34c0772ece0126dc9fb602562d9995da6cc
rahulniraula2 <rahulniraula47@gmail.com>
2024-02-16 02:25:45 -0800

    updating Building datatype example

--- models/Building.tsx
+++ models/Building.tsx
@@ -1,4 +1,4 @@
-import { FoodVendor, foodVendorExample } from './FoodVendor';
+import { FoodVendor, foodVendorExamples } from './FoodVendor';
 import Coordinates from './Coordinates'
 
 type Building = {
@@ -9,15 +9,17 @@ type Building = {
     vendors: FoodVendor[];
 };
 
-const coveBuildingExample = {
+const coveBuildingExample : Building = {
     name: "Building One",
     description: "Dining facility featuring a 700 person seating capacity across two levels with an open concept and food court",
     image: "https://www.uvic.ca/services/food/assets/images/cove-stairs",
     location: {
         latitude: 48.46421240,
         longitude: -123.30723430,
-      },
-    vendors: [foodVendorExample]
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+    },
+    vendors: foodVendorExamples
 }
 
 export {Building, coveBuildingExample}
