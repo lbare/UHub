3f3f55d021f96555096d99ad99c4cff8108d1faf
rahulniraula2 <57702423+rahulniraula2@users.noreply.github.com>
2024-03-03 19:15:06 -0800

    Enabled data fetch from firebase (#50)
    * enabled data fetching from firebase
    
    * removing unnecessary log

---
+++ services/DataFetcher.tsx
@@ -0,0 +1,48 @@
+import { collection, getDocs, doc } from "firebase/firestore"; 
+import {db} from "../src/firebase";
+import { Building, buildingExamples } from "../models/Building";
+import { FoodVendor } from "../models/FoodVendor";
+
+class DataFetcher {
+    collectionName: string;
+
+    //Our app is making excessive calls to the database, quick fix to cache the data
+    //need to come up with a better solution for milestone 2
+    buildings_cache : Building[] = [];
+
+    fetch_count : number = 0;
+  
+    constructor(collectionName: string = "Building") {
+      this.collectionName = collectionName;
+    }
+  
+    getAllBuildings(success: (data: Building[]) => void) {
+
+      this.fetch_count++;
+      console.log("Fetching buildings: ", this.fetch_count);
+      
+      if (this.buildings_cache.length > 0) {
+        success(this.buildings_cache);
+        return;
+      }
+      const dataCollection = collection(db, this.collectionName);
+      getDocs(dataCollection)
+        .then( documents => {
+          const buildings = documents.docs.map(doc => doc.data() as Building);
+          this.buildings_cache = buildings;
+          success(buildings);
+        })
+        .catch(error => {
+          console.error("Error fetching buildings: ", error);
+        });
+
+  }
+
+  getAllFoodVendorsInBuilding(building: Building) : FoodVendor[] {
+      return building.vendors;
+  }
+}
+
+export default DataFetcher;
+
+
