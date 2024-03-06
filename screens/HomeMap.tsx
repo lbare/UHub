bf365854b6352ca22a21962aafc6826bd0c83d46
Levi <47727379+lbare@users.noreply.github.com>
2024-03-05 17:43:53 -0800

    Fix view blocking map

--- screens/HomeMap.tsx
+++ screens/HomeMap.tsx
@@ -403,8 +403,6 @@ const HomeMap: React.FC = () => {
               right: 0,
               zIndex: 1,
               backgroundColor: "transparent",
-              width: "100%",
-              height: "100%",
             }}
           >
             <SearchBar
