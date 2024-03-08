01f4ee24847615e2b1ee0823eb8ee324f8c5ee0e
rahulniraula2 <57702423+rahulniraula2@users.noreply.github.com>
2024-03-07 19:34:58 -0800

    UI improvements ms2 (#59)
    * Change map styling
    
    * Added search bar with results
    
    ---------
    
    Co-authored-by: Levi <47727379+lbare@users.noreply.github.com>
    Co-authored-by: rahulniraula2 <rahulniraula47@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeNavigation from "./navigation/HomeNavigation";
+import { Building } from "./models/Building";
+import { getDocs, doc, collection } from "firebase/firestore";
+import { BuildingContext } from "./contexts/BuildingContext";
+import { db } from "./services/firebase";
 
 export default function App() {
+  const [buildings, setBuildings] = useState<Building[]>([]);
+
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      try {
+        const buildingsColRef = collection(db, "Building");
+        const querySnapshot = await getDocs(buildingsColRef);
+
+        const buildingsArray: Building[] = [];
+
+        querySnapshot.forEach((doc) => {
+          const buildingData = doc.data() as Building;
+          buildingsArray.push(buildingData);
+        });
+
+        setBuildings(buildingsArray);
+        console.log("Buildings fetched:", buildingsArray);
+      } catch (error) {
+        console.error("Error fetching buildings:", error);
+      }
+    };
+
+    fetchBuildings();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <HomeNavigation />
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <BuildingContext.Provider value={buildings}>
+      <NavigationContainer>
+        <HomeNavigation />
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </BuildingContext.Provider>
   );
 }
