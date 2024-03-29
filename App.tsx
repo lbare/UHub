e1ae7b53b6c72407efa609cd6e12efe3405b570b
lbare <levi.bare@gmail.com>
2024-03-28 18:46:26 -0700

    Deal with lastUpdated timestamp in firebase

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeNavigation from "./navigation/HomeNavigation";
 import { Building } from "./models/Building";
-import { getDocs, collection } from "firebase/firestore";
+import { getDoc, getDocs, doc, collection } from "firebase/firestore";
 import { BuildingContext } from "./contexts/BuildingContext";
 import { db } from "./services/firebase";
 import loadAssets from "./hooks/loadAssets";
@@ -15,49 +15,58 @@ export default function App() {
   const [buildings, setBuildings] = useState<Building[]>([]);
   const [isReady, setIsReady] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      await loadAssets();
-      const cache = await AsyncStorage.getItem("buildings_cache");
-      const now = new Date();
-
-      if (cache) {
-        const { data, timestamp } = JSON.parse(cache);
-        const cacheAge = now.getTime() - timestamp;
-
-        if (cacheAge < 86400000) {
-          // 24 hours in milliseconds
-          setBuildings(data);
-          console.log("Using cached data");
-        } else {
-          await updateDataFromFirebase();
-        }
-      } else {
-        await updateDataFromFirebase();
-      }
-    } catch (error) {
-      console.error("Error fetching buildings:", error);
-    } finally {
-      setIsReady(true);
-    }
-  };
-
-  const updateDataFromFirebase = async () => {
-    const querySnapshot = await getDocs(collection(db, "Building:V2"));
-    const buildingsArray = querySnapshot.docs.map(
-      (doc) => doc.data() as Building
-    );
-
-    setBuildings(buildingsArray);
-    await AsyncStorage.setItem(
-      "buildings_cache",
-      JSON.stringify({ data: buildingsArray, timestamp: new Date().getTime() })
-    );
-    console.log("Using new data");
-  };
-
   useEffect(() => {
-    fetchData();
+    const prepare = async () => {
+      try {
+        await loadAssets();
+
+        // Fetch the last updated timestamp from Firestore
+        const metadataDoc = await getDoc(doc(db, "Metadata", "lastUpdated"));
+        const serverTimestamp = metadataDoc.data()?.timestamp;
+        const serverLastUpdated = serverTimestamp
+          ? serverTimestamp.seconds * 1000 +
+            serverTimestamp.nanoseconds / 1000000
+          : null;
+        console.log("Server last updated:", serverLastUpdated ? new Date(serverLastUpdated).toLocaleString() : "N/A");
+
+        const localData = await AsyncStorage.getItem("buildings_cache");
+        const localCache = localData ? JSON.parse(localData) : {};
+        const localLastUpdated = localCache.lastUpdated || null;
+        console.log("Local last updated:", localLastUpdated ? new Date(localLastUpdated).toLocaleString() : "N/A");
+
+        // Check if the Firestore data is newer than the local cache
+        if (
+          !localData ||
+          !localLastUpdated ||
+          (serverLastUpdated && serverLastUpdated > localLastUpdated)
+        ) {
+          // Data in Firestore is newer, fetch and cache it
+          const querySnapshot = await getDocs(collection(db, "Building:V2"));
+          const buildingsArray = querySnapshot.docs.map(
+            (doc) => doc.data() as Building
+          );
+          setBuildings(buildingsArray);
+          await AsyncStorage.setItem(
+            "buildings_cache",
+            JSON.stringify({
+              data: buildingsArray,
+              lastUpdated: serverLastUpdated,
+            })
+          );
+          console.log("Buildings updated from Firestore");
+        } else {
+          // Load data from the local cache
+          setBuildings(JSON.parse(localData).data);
+          console.log("Buildings loaded from cache");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    prepare();
   }, []);
 
   if (!isReady) {
