f08a1189f4b7f4119c8a417a2d4ee4c2f8975f0e
Bipasa Agrawal <bipasaagrawal@uvic.ca>
2024-03-30 06:04:20 -0700

    UI design and removing mock account

--- services/Firebase/firebase-auth.tsx
+++ services/Firebase/firebase-auth.tsx
@@ -11,16 +11,6 @@ import {
 
 class FirebaseAuthManager {
 
-  // private mocked: boolean;
-
-  // constructor(mocked: boolean = false, callbackOnAuthStateChanged?: (user: User | null) => void) {
-  //   if (callbackOnAuthStateChanged) {
-  //     onAuthStateChanged(this.getCurrentAuth(), (user) => {
-  //       callbackOnAuthStateChanged(user);
-  //     })
-  //   }
-  //   this.mocked = mocked;
-  // }
   constructor(callbackOnAuthStateChanged?: (user: User | null) => void) {
     if (callbackOnAuthStateChanged) {
       onAuthStateChanged(this.getCurrentAuth(), (user) => {
@@ -44,10 +34,6 @@ class FirebaseAuthManager {
   }
 
   public getCurrentUserUID(): string | null {
-    
-    // if (this.mocked) {
-    //   return "rahul@uvic.ca";
-    // }
 
     try {
       return this.getCurrentUser().uid;
