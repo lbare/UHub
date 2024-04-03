141c6e4889173840af95f83ff33c01fc9d1c93a1
Olivier Gervais-Gougeon <76759158+olivier361@users.noreply.github.com>
2024-04-02 17:41:12 -0700

    Refactor pages to array

--- components/WelcomePopup.tsx
+++ components/WelcomePopup.tsx
@@ -12,7 +12,6 @@ export const WelcomePopup: React.FC<WelcomePopupProps> = ({
   onClose,
 }) => {
   const [curPageNum, setCurPageNum] = useState<number>(pageNum);
-  const TOTAL_PAGE_COUNT = 2;
 
   const nextPage = () => {
     setCurPageNum(curPageNum + 1);
@@ -27,89 +26,131 @@ export const WelcomePopup: React.FC<WelcomePopupProps> = ({
     onClose();
   };
 
+  const pages = [
+    /// Page 0 - Category Menu ///
+    <View className="bg-white rounded-lg shadow-lg p-4 w-3/4 h-1/3 flex-column justify-center items-center">
+      <Pressable
+        className="bg-orange w-3/4 p-2 h-12 rounded-lg justify-center items-center"
+        onPress={() => {
+          gotoPage(1);
+        }}
+      >
+        <Text className="text-white font-bold text-center text-lg">
+          What's New
+        </Text>
+      </Pressable>
+      <Pressable
+        className="bg-orange w-3/4 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+        onPress={() => {
+          gotoPage(1);
+        }}
+      >
+        <Text className="text-white font-bold text-center text-lg">
+          Welcome Tour
+        </Text>
+      </Pressable>
+      <Pressable
+        className="border-orange border-2 w-3/4 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+        onPress={closePopup}
+      >
+        <Text className="text-orange font-bold text-center text-lg">Close</Text>
+      </Pressable>
+    </View>,
+
+    /// Page 1 - Welcome tour intro ///
+    <View className="bg-white rounded-lg shadow-lg p-4 w-5/6 h-2/3 items-center">
+      <Image
+        source={require("../assets/full-logo.png")}
+        style={{
+          maxWidth: "80%",
+          maxHeight: "20%",
+          objectFit: "contain",
+          // borderWidth: 2, // TODO: for testing
+          // borderColor: "black",
+        }}
+      />
+      <View className="w-full">
+        <Text className="text-left text-xl font-bold mt-4">
+          Welcome to UHub!
+        </Text>
+        <Text className="text-left text-base mt-4">
+          UHub is a centralized platform to discover and explore resources on
+          campus.
+          {"\n"}
+          {"\n"}
+          UHub, allows you to find the locations of food outlets on campus and
+          browse their menus. There are also various search options available to
+          find specific food items, dietary restrictions, and more.
+        </Text>
+      </View>
+      <View className="flex-row justify-items-center space-x-7">
+        <Pressable
+          className="border-orange border-2 w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+          onPress={closePopup}
+        >
+          <Text className="text-orange font-bold text-center text-lg">
+            Close
+          </Text>
+        </Pressable>
+        <Pressable
+          className="bg-orange w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+          onPress={nextPage}
+        >
+          <Text className="text-white font-bold text-center text-lg">Next</Text>
+        </Pressable>
+      </View>
+    </View>,
+
+    /// Page 2 - Welcome tour browse vendors ///
+    <View className="bg-white rounded-lg shadow-lg p-4 w-5/6 h-2/3 items-center">
+      <Image
+        source={require("../assets/full-logo.png")}
+        style={{
+          maxWidth: "80%",
+          maxHeight: "20%",
+          objectFit: "contain",
+          // borderWidth: 2, // TODO: for testing
+          // borderColor: "black",
+        }}
+      />
+      <View className="w-full">
+        <Text className="text-left text-xl font-bold mt-4">
+          Browse Vendors!
+        </Text>
+        <Text className="text-left text-base mt-4">
+          You can browse vendors, wow!
+          {"\n"}
+          {"\n"}
+          UHub, allows you to find the locations of food outlets on campus and
+          browse their menus. There are also various search options available to
+          find specific food items, dietary restrictions, and more.
+        </Text>
+      </View>
+      <View className="flex-row justify-items-center space-x-7">
+        <Pressable
+          className="border-orange border-2 w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+          onPress={closePopup}
+        >
+          <Text className="text-orange font-bold text-center text-lg">
+            Close
+          </Text>
+        </Pressable>
+        <Pressable
+          className="bg-orange w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
+          onPress={nextPage}
+        >
+          <Text className="text-white font-bold text-center text-lg">Next</Text>
+        </Pressable>
+      </View>
+    </View>,
+  ];
+
   return (
     <Modal visible={isVisible} transparent={true} animationType="slide">
       <View className="flex-1 justify-center items-center">
-        {curPageNum === 0 && (
-          <View className="bg-white rounded-lg shadow-lg p-4 w-3/4 h-1/3 flex-column justify-center items-center">
-            <Pressable
-              className="bg-orange w-3/4 p-2 h-12 rounded-lg justify-center items-center"
-              onPress={() => {
-                gotoPage(1);
-              }}
-            >
-              <Text className="text-white font-bold text-center text-lg">
-                What's New
-              </Text>
-            </Pressable>
-            <Pressable
-              className="bg-orange w-3/4 p-2 h-12 rounded-lg mt-4 justify-center items-center"
-              onPress={() => {
-                gotoPage(1);
-              }}
-            >
-              <Text className="text-white font-bold text-center text-lg">
-                Welcome Tour
-              </Text>
-            </Pressable>
-            <Pressable
-              className="border-orange border-2 w-3/4 p-2 h-12 rounded-lg mt-4 justify-center items-center"
-              onPress={closePopup}
-            >
-              <Text className="text-orange font-bold text-center text-lg">
-                Close
-              </Text>
-            </Pressable>
-          </View>
-        )}
-        {curPageNum === 1 && (
-          <View className="bg-white rounded-lg shadow-lg p-4 w-5/6 h-2/3 items-center">
-            <Image
-              source={require("../assets/full-logo.png")}
-              style={{
-                maxWidth: "80%",
-                maxHeight: "20%",
-                objectFit: "contain",
-                // borderWidth: 2, // TODO: for testing
-                // borderColor: "black",
-              }}
-            />
-            <View className="w-full">
-              <Text className="text-left text-xl font-bold mt-4">
-                Welcome to UHub!
-              </Text>
-              <Text className="text-left text-base mt-4">
-                UHub is a centralized platform to discover and explore resources
-                on campus.
-                {"\n"}
-                {"\n"}
-                UHub, allows you to find the locations of food outlets on campus
-                and browse their menus. There are also various search options
-                available to find specific food items, dietary restrictions, and
-                more.
-              </Text>
-            </View>
-            <View className="flex-row justify-items-center space-x-7">
-              <Pressable
-                className="border-orange border-2 w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
-                onPress={closePopup}
-              >
-                <Text className="text-orange font-bold text-center text-lg">
-                  Close
-                </Text>
-              </Pressable>
-              <Pressable
-                className="bg-orange w-1/3 p-2 h-12 rounded-lg mt-4 justify-center items-center"
-                onPress={nextPage}
-              >
-                <Text className="text-white font-bold text-center text-lg">
-                  Next
-                </Text>
-              </Pressable>
-            </View>
-          </View>
-        )}
-        {(curPageNum < 0 || curPageNum >= TOTAL_PAGE_COUNT) && (
+        {curPageNum >= 0 && curPageNum < pages.length ? (
+          pages[curPageNum]
+        ) : (
           <View className="bg-white rounded-lg shadow-lg p-4 w-5/6 h-2/3 justify-center items-center">
             <View className="w-full">
               <Text className="text-left text-xl font-bold mt-4">
